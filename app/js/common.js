5a874a9f3fd1e05706f746ab9fc75f97da314f1c
ununoctium1187 <ununoctium1187@googlemail.com>
2017-09-12 01:00:29 +0300

    Convert img to svg

--- app/js/common.js
+++ app/js/common.js
@@ -1,7 +1,9 @@
 $(function() {
 	// Custom JS
+  ReplaceSVGWithInline();
   Onclick();
   GetReleases("qvepoy/ScreenGrabber");
+
 });
 
 function GetReleases(repo) {
@@ -38,4 +40,38 @@ function Onclick() {
   $( "#donate" ).click(function() {
     $( "#donates" ).toggleClass('hidden');
   });
+}
+
+function ReplaceSVGWithInline() {
+  /*
+    * Replace all SVG images with inline SVG
+    */
+       jQuery('img.svg').each(function(){
+           var $img = jQuery(this);
+           var imgID = $img.attr('id');
+           var imgClass = $img.attr('class');
+           var imgURL = $img.attr('src');
+
+           jQuery.get(imgURL, function(data) {
+               // Get the SVG tag, ignore the rest
+               var $svg = jQuery(data).find('svg');
+
+               // Add replaced image's ID to the new SVG
+               if(typeof imgID !== 'undefined') {
+                   $svg = $svg.attr('id', imgID);
+               }
+               // Add replaced image's classes to the new SVG
+               if(typeof imgClass !== 'undefined') {
+                   $svg = $svg.attr('class', imgClass+' replaced-svg');
+               }
+
+               // Remove any invalid XML tags as per http://validator.w3.org
+               $svg = $svg.removeAttr('xmlns:a');
+
+               // Replace image with new SVG
+               $img.replaceWith($svg);
+
+           }, 'xml');
+
+       });
 }
